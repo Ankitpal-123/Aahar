3495759c48097798b0f1e5d0925c3247bc3a2549
Ankitpal-123 <pal787871@gmail.com>
2022-01-29 17:28:18 +0530

    nav day 1

---
+++ client/src/Components/Navbar.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <>
+      <nav className="bg-black  py-4 grid grid-cols-12 text-white h-20 mx-100">
+        <div className="col-span-2">
+          <img
+            src="https://gdurl.com/0PTl"
+            alt="aahar"
+            className="h-20  ml-20 -mt-3  "></img>
+        </div>
+        <div className="flex ml-15 col-span-4 justify-between  mt-2 text-lg font-bold">
+          <a href="">HOME</a>
+          <p>·</p>
+          <a href="">MENU</a>
+          <p>·</p>
+          <a href="">ORDER</a>
+          <p>·</p>
+          <a href="">NEAR ME</a>
+          <p>·</p>
+          <a href="">ABOUT</a>
+        </div>
+        <div className="col-span-3"></div>
+        <div className="flex col-span-2 justify-between mt-2 text-lg font-bold ">
+          <a href="">ADD RESTURANT</a>
+          <p>·</p>
+          <a href="">REGISTER</a>
+        </div>
+      </nav>
+    </>
+  );
+};
+
+export default Navbar;
---
+++ client/src/screens/Landing.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Navbar from "../Components/Navbar";
+
+const landing = () => {
+  return (
+    <div>
+      <Navbar />
+    </div>
+  );
+};
+
+export default landing;
